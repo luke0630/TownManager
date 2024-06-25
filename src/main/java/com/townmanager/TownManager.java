0e0138986cdfeb3d408e208f315b096de4cecfeb
luke0630 <ryuto-0915@outlook.jp>
2024-06-25 11:13:29 +0900

    first commit

---
+++ src/main/java/com/townmanager/TownManager.java
@@ -0,0 +1,20 @@
+package com.townmanager;
+
+import com.townprotection.TownProtection;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TownManager extends JavaPlugin {
+
+    private TownProtection api = (TownProtection) Bukkit.getServer().getPluginManager().getPlugin("TownProtection");
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
